77152e74eb042b97fecb64daf233dd9578d750e2
Cam Kennedy <cameron.kennedy@airbyte.io>
2026-05-13 10:39:08 -0700

    only print the version number (#12)

--- cmd/version.go
+++ cmd/version.go
@@ -1,9 +1,7 @@
 package cmd
 
 import (
-	"encoding/json"
 	"fmt"
-	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -18,20 +16,12 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the CLI version",
 	Run: func(cmd *cobra.Command, args []string) {
-		info := map[string]string{
-			"version": Version,
-			"commit":  Commit,
-			"date":    Date,
-		}
-		enc := json.NewEncoder(os.Stdout)
-		enc.SetIndent("", "  ")
-		if err := enc.Encode(info); err != nil {
-			fmt.Fprintf(os.Stderr, "error encoding version: %v\n", err)
-			os.Exit(1)
-		}
+		fmt.Println(Version)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(versionCmd)
+	rootCmd.Version = Version
+	rootCmd.SetVersionTemplate("{{.Version}}\n")
 }
